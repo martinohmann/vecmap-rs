1b2fcb5cc4aaa8e204fa03d3e03b684dc26281f5
martinohmann <martinohmann@gmail.com>
2025-01-26 01:37:33 +0100

    chore: this should be the standard

--- src/map/slice.rs
+++ src/map/slice.rs
@@ -8,18 +8,21 @@ pub struct Slice<K, V> {
     entries: [Slot<K, V>],
 }
 
-// SAFETY: `Slice<K, V>` is a transparent wrapper around `[Slot<K, V>]`, and reference lifetimes
-// are bound together in function signatures.
 impl<K, V> Slice<K, V> {
     pub(super) const fn from_slice(entries: &[Slot<K, V>]) -> &Slice<K, V> {
+        // SAFETY: `&[Slot<K, V>]` and `&Slice<K, V>` have the same memory layout because `Slice<K, V>` is
+        // just a transparent wrapper.
         unsafe { &*(ptr::from_ref::<[Slot<K, V>]>(entries) as *const Slice<K, V>) }
     }
 
     pub(super) const fn from_mut_slice(entries: &mut [Slot<K, V>]) -> &mut Slice<K, V> {
+        // SAFETY: `&mut [Slot<K, V>]` and `&mut Slice<K, V>` have the same memory layout because `Slice<K, V>` is
+        // just a transparent wrapper.
         unsafe { &mut *(ptr::from_mut::<[Slot<K, V>]>(entries) as *mut Slice<K, V>) }
     }
 
     pub(super) fn from_boxed(entries: Box<[Slot<K, V>]>) -> Box<Slice<K, V>> {
+        // SAFETY: `A [Slot<K, V>]` and a `Slice<K, V>` are essentially the same thing.
         unsafe { Box::from_raw(Box::into_raw(entries) as *mut Slice<K, V>) }
     }
 
--- src/set/slice.rs
+++ src/set/slice.rs
@@ -8,18 +8,21 @@ pub struct Slice<T> {
     entries: [Slot<T>],
 }
 
-// SAFETY: `Slice<T>` is a transparent wrapper around `[Slot<T>]`, and reference lifetimes are
-// bound together in function signatures.
 impl<T> Slice<T> {
     pub(super) const fn from_slice(entries: &[Slot<T>]) -> &Self {
+        // SAFETY: `&[Slot<T>]` and `&Slice<T>` have the same memory layout because `Slice<T>` is
+        // just a transparent wrapper.
         unsafe { &*(ptr::from_ref::<[Slot<T>]>(entries) as *const Self) }
     }
 
     pub(super) const fn from_mut_slice(entries: &mut [Slot<T>]) -> &mut Self {
+        // SAFETY: `&mut [Slot<T>]` and `&mut Slice<T>` have the same memory layout because `Slice<T>` is
+        // just a transparent wrapper.
         unsafe { &mut *(ptr::from_mut::<[Slot<T>]>(entries) as *mut Self) }
     }
 
     pub(super) fn from_boxed(entries: Box<[Slot<T>]>) -> Box<Self> {
+        // SAFETY: `A [Slot<T>]` and a `Slice<T>` are essentially the same thing.
         unsafe { Box::from_raw(Box::into_raw(entries) as *mut Self) }
     }
 
