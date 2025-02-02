f43de62486cb491f5904a02648c2a1f843535c2c
martinohmann <martinohmann@gmail.com>
2025-02-02 17:16:04 +0100

    chore: add slice index tests

--- src/map/slice/impls.rs
+++ src/map/slice/impls.rs
@@ -169,7 +169,9 @@ impl<'a, K, V> IntoIterator for &'a mut MapSlice<K, V> {
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use crate::vecmap;
+    use alloc::vec::Vec;
 
     #[test]
     fn eq() {
@@ -189,5 +191,112 @@ mod test {
         assert_ne!(map.as_map_slice(), &[("c", 3), ("b", 2), ("a", 1)]);
         assert_ne!(map.as_map_slice(), [("c", 3), ("b", 2), ("a", 1)]);
         assert_ne!(map.as_mut_map_slice(), [("c", 3), ("b", 2), ("a", 1)]);
+    }
+
+    #[test]
+    fn slice_index() {
+        fn check(
+            vec_slice: &[(i32, i32)],
+            map_slice: &MapSlice<i32, i32>,
+            sub_slice: &MapSlice<i32, i32>,
+        ) {
+            assert_eq!(map_slice, sub_slice);
+            assert!(vec_slice
+                .iter()
+                .copied()
+                .eq(map_slice.iter().map(|(&k, &v)| (k, v))));
+            assert!(vec_slice.iter().map(|(k, _)| k).eq(map_slice.keys()));
+            assert!(vec_slice.iter().map(|(_, v)| v).eq(map_slice.values()));
+        }
+
+        let vec: Vec<(i32, i32)> = (0..10).map(|i| (i, i * i)).collect();
+        let map: VecMap<i32, i32> = vec.iter().cloned().collect();
+        let slice = map.as_map_slice();
+
+        // RangeFull
+        check(&vec[..], &map[..], &slice[..]);
+
+        for i in 0usize..10 {
+            // Index
+            assert_eq!(vec[i].1, map[i]);
+            assert_eq!(vec[i].1, slice[i]);
+            assert_eq!(map[&(i as i32)], map[i]);
+            assert_eq!(map[&(i as i32)], slice[i]);
+
+            // RangeFrom
+            check(&vec[i..], &map[i..], &slice[i..]);
+
+            // RangeTo
+            check(&vec[..i], &map[..i], &slice[..i]);
+
+            // RangeToInclusive
+            check(&vec[..=i], &map[..=i], &slice[..=i]);
+
+            // (Bound<usize>, Bound<usize>)
+            let bounds = (Bound::Excluded(i), Bound::Unbounded);
+            check(&vec[i + 1..], &map[bounds], &slice[bounds]);
+
+            for j in i..=10 {
+                // Range
+                check(&vec[i..j], &map[i..j], &slice[i..j]);
+            }
+
+            for j in i..10 {
+                // RangeInclusive
+                check(&vec[i..=j], &map[i..=j], &slice[i..=j]);
+            }
+        }
+    }
+
+    #[test]
+    fn slice_index_mut() {
+        fn check_mut(
+            vec_slice: &[(i32, i32)],
+            map_slice: &mut MapSlice<i32, i32>,
+            sub_slice: &mut MapSlice<i32, i32>,
+        ) {
+            assert_eq!(map_slice, sub_slice);
+            assert!(vec_slice
+                .iter()
+                .copied()
+                .eq(map_slice.iter_mut().map(|(&k, &mut v)| (k, v))));
+            assert!(vec_slice.iter().map(|(_, v)| v).eq(map_slice.values_mut()));
+        }
+
+        let vec: Vec<(i32, i32)> = (0..10).map(|i| (i, i * i)).collect();
+        let mut map: VecMap<i32, i32> = vec.iter().cloned().collect();
+        let mut map2 = map.clone();
+        let slice = map2.as_mut_map_slice();
+
+        // RangeFull
+        check_mut(&vec[..], &mut map[..], &mut slice[..]);
+
+        for i in 0usize..10 {
+            // IndexMut
+            assert_eq!(&mut map[i], &mut slice[i]);
+
+            // RangeFrom
+            check_mut(&vec[i..], &mut map[i..], &mut slice[i..]);
+
+            // RangeTo
+            check_mut(&vec[..i], &mut map[..i], &mut slice[..i]);
+
+            // RangeToInclusive
+            check_mut(&vec[..=i], &mut map[..=i], &mut slice[..=i]);
+
+            // (Bound<usize>, Bound<usize>)
+            let bounds = (Bound::Excluded(i), Bound::Unbounded);
+            check_mut(&vec[i + 1..], &mut map[bounds], &mut slice[bounds]);
+
+            for j in i..=10 {
+                // Range
+                check_mut(&vec[i..j], &mut map[i..j], &mut slice[i..j]);
+            }
+
+            for j in i..10 {
+                // RangeInclusive
+                check_mut(&vec[i..=j], &mut map[i..=j], &mut slice[i..=j]);
+            }
+        }
     }
 }
